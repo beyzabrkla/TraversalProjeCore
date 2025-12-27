7453c7500f70968053314aaf97e8477423cc0f51
Beyza Rabia Kılıçaslan <beyzarabia121@gmail.com>
2025-12-27 19:57:54 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Feature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EntityLayer.Concrete
+{
+    public class Feature
+    {
+        [Key]
+        public int FeatureId { get; set; }
+        public string? Post1Name { get; set; }
+        public string? Post1Description { get; set; }
+        public string? Post1Image { get; set; }
+        public string? Status { get; set; }
+    }
+}
