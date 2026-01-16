21c6bbbbc50f63568cf8696e7a02661fbe7680a9
Beyza Rabia Kılıçaslan <beyzarabia121@gmail.com>
2026-01-16 16:59:01 +0300

    71.ders sonu RapidApi eklendi

---
+++ TraversalProjeCore/Areas/Admin/Controllers/APIExchangeController.cs
@@ -0,0 +1,39 @@
+﻿using DocumentFormat.OpenXml.Drawing.Charts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using TraversalProjeCore.Areas.Admin.Models;
+
+namespace TraversalProjeCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class APIExchangeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<BookingExchangeViewModel2> BookingExchangeViewModels = new List<BookingExchangeViewModel2>();
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?currency=TRY&locale=en-gb"),
+                Headers =
+    {
+        { "x-rapidapi-key", "b869be713fmsh1f7976a7f113390p175f7djsnd28c78181ba6" },
+        { "x-rapidapi-host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<BookingExchangeViewModel2>(body);
+                return View(values.exchange_rates);
+
+            }
+        }
+    }
+}
---
+++ TraversalProjeCore/Areas/Admin/Models/APIMovieViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TraversalProjeCore.Areas.Admin.Models
+{
+    public class APIMovieViewModel
+    {
+        public int rank { get; set; }
+        public string Title { get; set; }
+        public string Rating { get; set; }
+        public int Year { get; set; }
+        public string Trailer { get; set; }
+    }
+}
