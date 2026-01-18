0d3f1629e72ab4ff8b90c2ca2ab9f2649f22e21e
Beyza Rabia Kılıçaslan <beyzarabia121@gmail.com>
2026-01-18 21:54:13 +0300

    78

---
+++ TraversalProjeCore/ViewComponents/Destination/GuideDetails.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalProjeCore.ViewComponents.Destination
+{
+    public class GuideDetails : ViewComponent
+    {
+        private readonly IGuideService _guideService;
+
+        public GuideDetails(IGuideService guideService)
+        {
+            _guideService = guideService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values= _guideService.TGetById(1);
+            return View(values);
+        }
+    }
+}
